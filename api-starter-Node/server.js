1b8e27d62e5fa87e740865f463426361eb176ad2
Akshit Dhameliya <104002483+akshit2523@users.noreply.github.com>
2024-12-16 11:20:41 +0530

    Update server.js

--- api-starter-Node/server.js
+++ api-starter-Node/server.js
@@ -8,9 +8,9 @@ const morganBody = require('morgan-body')
 const { publicRoutes, privateRoutes } = require('./routes')
 const mongodb = require('./mongodb-config')
 const authentication = require('./middleware/Authentication')
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc')
-// const swaggerDocument = require('./swagger.json');
+
+const initialize = async () => {
+  await mongodb.getOrInitializeDatabase()
 
   app.use(express.json())
   app.use(cors())
@@ -19,94 +19,6 @@ const swaggerJsdoc = require('swagger-jsdoc')
     prettify: false,
     includeNewLine: true
   })
-
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'API Documentation',
-    version: '1.0.0',
-    description: 'API Information for MySoundsGlobal backend',
-  },
-  servers: [
-    {
-      url: `http://localhost:${constants.PORT}`, // Base URL for your API (adjust to your actual URL)
-    },
-  ],
-  components: {
-    schemas: {
-      Dummy: {
-        type: 'object',
-        properties: {
-          _id: {
-            type: 'string',
-            description: 'Unique identifier for the dummy',
-          },
-          title: {
-            type: 'string',
-            description: 'Title of the dummy',
-          },
-          markedAsRead: {
-            type: 'boolean',
-            description: 'Indicates if the dummy is marked as read',
-          },
-          markedAsView: {
-            type: 'boolean',
-            description: 'Indicates if the dummy is marked as viewed',
-          },
-          type: {
-            type: 'string',
-            description: 'Type of the dummy',
-          },
-          redirectLink: {
-            type: 'string',
-            description: 'Link to redirect',
-          },
-          // Add any other fields as needed
-        },
-      },
-      User: {
-        type: 'object',
-        properties: {
-          _id: {
-            type: 'string',
-            description: 'Unique identifier for the user',
-          },
-          desc: {
-            type: 'string',
-            description: 'Description of the user',
-          },
-          email: {
-            type: 'string',
-            description: 'Email address of the user',
-          },
-          password: {
-            type: 'string',
-            description: 'Password of the user',
-          },
-          type: {
-            type: 'string',
-            description: 'Type or role of the user',
-          },
-        },
-        required: ['_id', 'desc', 'email', 'password', 'type'],
-      },
-    },
-  },
-}
-
-const options = {
-  swaggerDefinition,
-  // Path to the API docs
-  apis: ['./routes/v1/*.js'], // Adjust this path according to where your route files are located
-}
-
-const swaggerSpec = swaggerJsdoc(options)
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-const initialize = async () => {
-  await mongodb.getOrInitializeDatabase()
-
 
   // Public Routes! i.e. Login, SignUp etc.
   app.use('/', publicRoutes)
@@ -120,7 +32,7 @@ const initialize = async () => {
     return res.status(500).json({ error: error.message || 'Something unexpected happened!' })
   })
 
-  app.listen(constants.PORT || 9090 , () => console.log(`Listening on port http://localhost:${constants.PORT}`))
+  app.listen(9090 || constants.PORT, () => console.log(`Listening on port http://localhost:${constants.PORT}`))
 }
 
 initialize().catch((error) => console.error('Error while setup server', error.message))
