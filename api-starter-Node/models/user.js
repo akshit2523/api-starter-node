93610e265d3f2a37530805b907b13f6ecf9a0001
Akshit Dhameliya <104002483+akshit2523@users.noreply.github.com>
2024-12-17 15:25:23 +0530

    Update user.js

--- api-starter-Node/models/user.js
+++ api-starter-Node/models/user.js
@@ -7,10 +7,8 @@ const { defaultFields, defaultSchemaOptions } = require('./model-utils')
 
 const userSchema = new Schema({
   _id: { type: Schema.Types.String, required: true, default: uuidv4 },
-  desc: { type: Schema.Types.String, required: true },
   email: { type: Schema.Types.String, required: true },
   password: { type: Schema.Types.String, required: true },
-  type: { type: Schema.Types.String, required: true },
   ...defaultFields
 }, {
   ...defaultSchemaOptions
